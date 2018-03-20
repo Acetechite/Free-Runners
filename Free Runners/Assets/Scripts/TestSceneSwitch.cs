9d8a0835ba11e29369b3ac4c23ce5f0c7f204b84
Acetechite <31084818+Acetechite@users.noreply.github.com>
2018-03-20 09:07:36 -0400

    Initial Commit
    Just added the assets for commit

---
+++ Free Runners/Assets/Scripts/TestSceneSwitch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TestSceneSwitch : MonoBehaviour {
+
+    public Button button;
+    // Use this for initialization
+    void Start () {
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(Settings);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Settings() {
+        SceneManager.LoadScene("Controls",LoadSceneMode.Additive);
+    }
+
+}
