a1dfa13d653f6ef3effe1e3decb7205f27c5d6b3
Ryan <rytymedeiros@gmail.com>
2018-03-26 13:42:01 -0400

    Added missing animations

--- Free Runners/Assets/Scripts/PlayerController.cs
+++ Free Runners/Assets/Scripts/PlayerController.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	public AudioSource audioSrc;
+	public SpriteRenderer playerSprite;
+	public Animator playerAnimator;
+
 	public SpriteRenderer spriteRen;
 	public Sprite late;
 	public Sprite perfect;
 	public Sprite early;
 	public string state;
+	AudioClip earlySound;
+	AudioClip perfectSound;
+	AudioClip lateSound;
+	Sprite playerWalk;
+	Sprite playerMiss;
+	RuntimeAnimatorController playerWalkAnim;
+	RuntimeAnimatorController playerMissAnim;
+
 	// Use this for initialization
 	void Start () {
 		state = "null";
+
+		earlySound = (AudioClip)Resources.Load("sounds/EarlySound");
+		perfectSound = (AudioClip)Resources.Load("sounds/PerfectSound");
+		lateSound = (AudioClip)Resources.Load("sounds/LateSound");
+
+		playerWalk = Resources.Load<Sprite> ("sprites/PlayerWalk");
+		playerMiss = Resources.Load<Sprite> ("sprites/PlayerMiss");
+
+		playerWalkAnim = Resources.Load<RuntimeAnimatorController> ("sprites/PlayerWalkAnim");
+		playerMissAnim = Resources.Load<RuntimeAnimatorController> ("sprites/PlayerMissAnim");
 	}
 	
 	// Update is called once per frame
@@ -26,12 +48,19 @@ public class PlayerController : MonoBehaviour {
 		case "null":
 			break;
 		case "late":
+			audioSrc.PlayOneShot (lateSound);
+			playerSprite.sprite = playerMiss;
+			playerAnimator.runtimeAnimatorController = playerMissAnim;
 			spriteRen.sprite = late;
 			break;
 		case "perfect":
+			audioSrc.PlayOneShot (perfectSound);
 			spriteRen.sprite = perfect;
 			break;
 		case "early":
+			audioSrc.PlayOneShot (earlySound);
+			playerSprite.sprite = playerMiss;
+			playerAnimator.runtimeAnimatorController = playerMissAnim;
 			spriteRen.sprite = early;
 			break;
 		}
@@ -39,7 +68,9 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	IEnumerator ShowState(){
-		yield return new WaitForSeconds (0.5f);
+		yield return new WaitForSeconds (0.8f);
+		playerSprite.sprite = playerWalk;
+		playerAnimator.runtimeAnimatorController = playerWalkAnim;
 		spriteRen.sprite = null;
 	}
 }
