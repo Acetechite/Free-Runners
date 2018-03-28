4b4a33146e8b8b063fe33d6c77edc3452ac3b1e1
Acetechite <31084818+Acetechite@users.noreply.github.com>
2018-03-28 17:51:11 -0400

    sized for build
    still need to size the game itself

--- Free Runners/Assets/Scripts/SceneController.cs
+++ Free Runners/Assets/Scripts/SceneController.cs
@@ -16,6 +16,8 @@ public class SceneController : MonoBehaviour {
         walkInPark = "0,2,3,2,2,3,3,2,3,3,3,2,2,4";
         constAhead = "3,2,1,2,2,2,1,1,3,2,1,3,1,4";
         hurdleHell = "0,1,1,0,1,1,1,2,1,0,1,1,1,4";
+
+        PlayerPrefs.SetInt("Pause", 0);
         
 	}
 	
